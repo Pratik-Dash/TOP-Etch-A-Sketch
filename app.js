bb28e74d59d5ec3659434a1a6b366eafe481c72a
Pratik Dash <dashpratik40@gmail.com>
2022-11-22 23:00:31 +0530

    Completed adding JS logic.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const uiContainer = document.querySelector(".container");
 const gridSizeSlider = document.querySelector(".divsizeslider");
+const resetButton = document.querySelector(".reset");
+const colorPicker = document.querySelector(".colorPicker");
+const rainbow = document.querySelector(".rainbow");
+const drawMessage = document.querySelector(".drawMessage");
+const eraseBtn = document.querySelector(".erase");
+let globalColor = "defaultColor";
 uiContainer.addEventListener("load",generateBoard(16))
+let pointerClicked = false;
 function generateBoard(size){
     
     uiContainer.style.gridTemplateColumns = `repeat(${size},1fr)`;
@@ -9,10 +16,40 @@ function generateBoard(size){
 
     for(let i = 0;i<totalNumberOfSquares;i++){
         let squareDiv = document.createElement("div");
-        squareDiv.style.border = "1px solid black";
+        //squareDiv.style.borderRadius = "30px"
+        squareDiv.addEventListener("pointerdown",()=>{
+
+            pointerClicked = !pointerClicked
+          
+        })
+        squareDiv.addEventListener("pointermove",()=>{
+            
+            if(pointerClicked === true){
+
+                if(globalColor === "color"){
+                    let colorPicked = colorPicker.value;
+                    squareDiv.style.backgroundColor = colorPicked;
+                    }
+                    else if(globalColor === "defaultColor"){
+                        squareDiv.style.backgroundColor = "#3d4143";
+                    }
+                    else if(globalColor === "Erase"){
+                        squareDiv.style.backgroundColor = "";
+                    }
+                    else {
+                        
+                        let r = Math.random() * 256;
+                        let g = Math.random() * 256;
+                        let b = Math.random() * 256;
+                        squareDiv.style.backgroundColor = `rgb(${r},${g},${b})`;
+        
+                    }
+            }
+        })
         uiContainer.insertAdjacentElement("beforeend",squareDiv);
     }
 }
+
 
 
 gridSizeSlider.addEventListener("change",(e)=>{
@@ -20,6 +57,33 @@ gridSizeSlider.addEventListener("change",(e)=>{
     uiContainer.innerHTML = "";
     generateBoard(e.target.value);
 })
+
+resetButton.addEventListener("click",()=>{
+
+    uiContainer.innerHTML = "";
+    generateBoard(16);
+    gridSizeSlider.value = 16;
+    globalColor = "defaultColor"
+    colorPicker.value = "#3d4143"
+    pointerClicked = "false";
+});
+colorPicker.addEventListener("click",()=>{
+
+    globalColor = "color";
+})
+rainbow.addEventListener("click",()=>{
+
+    globalColor = "rainbow";
+})
+eraseBtn.addEventListener("click",()=>{
+
+    globalColor = "Erase";
+})
+
+
+
+ 
+
 
 
 
