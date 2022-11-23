1b3efc0533a88fe4daed77c84d2f07de5d970370
Pratik Dash <dashpratik40@gmail.com>
2022-11-23 08:20:59 +0530

    Modified JS so user can draw with hold and drag.

--- app.js
+++ app.js
@@ -6,59 +6,63 @@ const rainbow = document.querySelector(".rainbow");
 const drawMessage = document.querySelector(".drawMessage");
 const eraseBtn = document.querySelector(".erase");
 let globalColor = "defaultColor";
-uiContainer.addEventListener("load",generateBoard(16))
-let pointerClicked = false;
-function generateBoard(size){
-    
+uiContainer.addEventListener("load", generateBoard(16))
+let pointerDown = false;
+function generateBoard(size) {
+
     uiContainer.style.gridTemplateColumns = `repeat(${size},1fr)`;
     uiContainer.style.gridTemplateRows = `repeat(${size},1fr)`;
     let totalNumberOfSquares = size * size;
 
-    for(let i = 0;i<totalNumberOfSquares;i++){
+    for (let i = 0; i < totalNumberOfSquares; i++) {
         let squareDiv = document.createElement("div");
-        //squareDiv.style.borderRadius = "30px"
         squareDiv.addEventListener("pointerdown",()=>{
 
-            pointerClicked = !pointerClicked
-          
+            pointerDown = true
         })
+           
         squareDiv.addEventListener("pointermove",()=>{
-            
-            if(pointerClicked === true){
 
-                if(globalColor === "color"){
+            if(pointerDown){
+
+                if (globalColor === "color") {
                     let colorPicked = colorPicker.value;
                     squareDiv.style.backgroundColor = colorPicked;
-                    }
-                    else if(globalColor === "defaultColor"){
-                        squareDiv.style.backgroundColor = "#3d4143";
-                    }
-                    else if(globalColor === "Erase"){
-                        squareDiv.style.backgroundColor = "";
-                    }
-                    else {
-                        
-                        let r = Math.random() * 256;
-                        let g = Math.random() * 256;
-                        let b = Math.random() * 256;
-                        squareDiv.style.backgroundColor = `rgb(${r},${g},${b})`;
-        
-                    }
+                }
+                else if (globalColor === "defaultColor") {
+                    squareDiv.style.backgroundColor = "#3d4143";
+                }
+                else if (globalColor === "Erase") {
+                    squareDiv.style.backgroundColor = "";
+                }
+                else {
+    
+                    let r = Math.random() * 256;
+                    let g = Math.random() * 256;
+                    let b = Math.random() * 256;
+                    squareDiv.style.backgroundColor = `rgb(${r},${g},${b})`;
+    
+                }
             }
         })
-        uiContainer.insertAdjacentElement("beforeend",squareDiv);
+        
+        squareDiv.addEventListener("pointerup",()=>{
+
+            pointerDown = false;
+        })
+        uiContainer.insertAdjacentElement("beforeend", squareDiv);
     }
 }
 
 
 
-gridSizeSlider.addEventListener("change",(e)=>{
-    
+gridSizeSlider.addEventListener("change", (e) => {
+
     uiContainer.innerHTML = "";
     generateBoard(e.target.value);
 })
 
-resetButton.addEventListener("click",()=>{
+resetButton.addEventListener("click", () => {
 
     uiContainer.innerHTML = "";
     generateBoard(16);
@@ -66,23 +70,35 @@ resetButton.addEventListener("click",()=>{
     globalColor = "defaultColor"
     colorPicker.value = "#3d4143"
     pointerClicked = "false";
+    rainbow.className = "button"
+    eraseBtn.className = "button"
+    colorPicker.className  = "button";
 });
-colorPicker.addEventListener("click",()=>{
+colorPicker.addEventListener("click", () => {
 
     globalColor = "color";
+    colorPicker.className  = "active";
+    rainbow.className = "button"
+    eraseBtn.className = "button"
 })
-rainbow.addEventListener("click",()=>{
+rainbow.addEventListener("click", () => {
 
     globalColor = "rainbow";
+    rainbow.className = "active"
+    colorPicker.className  = "button";
+    eraseBtn.className = "button"
 })
-eraseBtn.addEventListener("click",()=>{
+eraseBtn.addEventListener("click", () => {
 
     globalColor = "Erase";
+    eraseBtn.className = "active"
+    rainbow.className = "button"
+    colorPicker.className  = "button";
 })
 
 
 
- 
+
 
 
 
